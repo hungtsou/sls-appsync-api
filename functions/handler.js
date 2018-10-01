39bac13601e42a8afe295534ac6950607d9a7638
hungtsou <hungtsougg@gmail.com>
2018-10-01 05:40:45 -0600

    setup serverless with appsync and resources

---
+++ functions/handler.js
@@ -0,0 +1,19 @@
+export async function graphql(event, context, callback) {
+  console.log('Received event {}', JSON.stringify(event, 3));
+
+	const consumerKey = event.arguments.consumer_key;
+	const consumerSecret = event.arguments.consumer_secret;
+
+	console.log('Got an Invoke Request.');
+	switch (event.field) {
+		case 'helloWorld': {
+			callback(null, 'Hello world');
+			break;
+		}
+
+		default: {
+			callback(`Unknown field, unable to resolve ${event.field}`, null);
+			break;
+		}
+	}
+}
